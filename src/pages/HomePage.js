8b239cfc97f9f2711ad23d1d869699e89e282c74
Eyal Lev <eyal18yaffa03@gmail.com>
2020-01-17 22:43:12 +0200

    select breed is active

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
 import './HomePage.css';
 import { Jumbotron, DropdownButton, Dropdown } from 'react-bootstrap';
+import Axios from "axios";
+
 
 
 
 class HomePage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            dogbreeds: [],
+            value: 'select'
+        }
+        this.handleSelection = this.handleSelection.bind(this);
+    }
+    async   componentDidMount() {
+        // const res = await axios.get("https://dog.ceo/api/breeds/list/all");
+        // const breedNameArray = Object.keys(res.data.message);
+        // return breedNameArray
+
+        const res = await Axios.get('https://dog.ceo/api/breeds/list/all')
+        //  .then(response => {
+        const breedNameArray = Object.keys(res.data.message)
+        console.log(breedNameArray[0])
+        this.setState({
+            dogbreeds: breedNameArray
+
+        });
+        //  console.log(this.state.dogbreeds.constructor.name)
+        // })
+
+    }
+    handleSelection(event) {
+        // this.props.onSelectedResult(parseInt(ev.target.getAttribute("data-index")));
+        this.setState({ value: event.target.value });
+
     }
 
-
     render() {
+        const { dogbreeds } = this.state;
+        const listGroupItems = dogbreeds.map((dogtype, index) => {
+            return (<option value={dogtype} key={index} data-index={index} action onChange={this.handleSelection}> {dogtype}</option>);
+        })
         return (
             <div>
 
@@ -19,15 +51,14 @@ class HomePage extends Component {
                     <p>
                         Man's Best Firend
                     </p>
-                    <p>
-                        <DropdownButton id="dropdown-basic-button" title="Dog Breed">
-                            <Dropdown.Item href="#/action-1">Wolf</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                        </DropdownButton>
-                    </p>
+                    <select onChange={this.handleSelection} value={this.state.value} >
+                        {listGroupItems}
+                    </select>
+
+
+
                 </Jumbotron>
-            </div>
+            </div >
         );
     }
 }
