22f12fa67a0d62c99b6145a9705ba92587ed3abc
Eyal Lev <eyal18yaffa03@gmail.com>
2020-01-22 22:25:13 +0200

    Updates

---
+++ precache-manifest.ce19220892cc9d721f43ae08d7008805.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b133bf4fb8cbee420f7613caa99369dc",
+    "url": "/dog_book2/index.html"
+  },
+  {
+    "revision": "3be1b49225322387d2e3",
+    "url": "/dog_book2/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "7bc7ee350360931c8dfa",
+    "url": "/dog_book2/static/css/main.28339722.chunk.css"
+  },
+  {
+    "revision": "3be1b49225322387d2e3",
+    "url": "/dog_book2/static/js/2.aaa5f075.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/dog_book2/static/js/2.aaa5f075.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7bc7ee350360931c8dfa",
+    "url": "/dog_book2/static/js/main.c1664f3f.chunk.js"
+  },
+  {
+    "revision": "1bc253ef9309c5844a17",
+    "url": "/dog_book2/static/js/runtime-main.d14b0f6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dog_book2/static/media/logo.5d5d9eef.svg"
+  }
+]);
